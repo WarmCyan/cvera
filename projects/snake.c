cab85ad4c11c66ca29b69d96e1bb74ada748b1d4
Martindale, Nathan <nathanamartindale@gmail.com>
2024-12-22 12:41:11 -0500

    Got to point we can actually debug

--- projects/snake.c
+++ projects/snake.c
@@ -12,17 +12,71 @@
 
 #define BORDER_COLOR_BG 44
 #define BORDER_COLOR_FG 30
-
 #define SNAKE_COLOR_BG 42
-
 #define FOOD_COLOR_FG 33
+
+#define MSEC_PER_FRAME 1000
 
 static char cells[MAX_H][MAX_W];
 static int term_w, term_h;
 static int game_w, game_h;
 static int food_x, food_y;
+static clock_t last_frame;
+
+static int steps = 0;
 
 static struct termios original_term_attrs;
+
+void printout() {
+	if (snek_right) printf("snek_right ");
+	if (snek_x) printf("snek_x ");
+	if (snek_y) printf("snek_y ");
+	if (snek_bigness) printf("snek_bigness ");
+	if (setup) printf("setup ");
+	if (snek_tail_x) printf("snek_tail_x ");
+	if (snek_tail_y) printf("snek_tail_y ");
+	if (_o_clear_screen) printf("_o_clear_screen ");
+	if (_o_find_term_size) printf("_o_find_term_size ");
+	if (_o_prepare_frame_timer) printf("_o_prepare_frame_timer ");
+	if (_o_prepare_term_for_input) printf("_o_prepare_term_for_input ");
+	if (_o_generate_food) printf("_o_generate_food ");
+	if (_o_render_boundaries) printf("_o_render_boundaries ");
+	if (_o_render_snek) printf("_o_render_snek ");
+	if (frame_rendered) printf("frame_rendered ");
+	if (running) printf("running ");
+	if (_i_next_frame) printf("_i_next_frame ");
+	if (move_snek) printf("move_snek ");
+	if (_o_check_food) printf("_o_check_food ");
+	if (_i_food_get_happy_snek) printf("_i_food_get_happy_snek ");
+	if (embiggen_snek) printf("embiggen_snek ");
+	if (_i_no_food_get_sad_snek) printf("_i_no_food_get_sad_snek ");
+	if (_o_clear_last_tail) printf("_o_clear_last_tail ");
+	if (_o_check_ohnoes_ate_self) printf("_o_check_ohnoes_ate_self ");
+	if (_i_no_eat_self_good_snek) printf("_i_no_eat_self_good_snek ");
+	if (_i_OHNOES_ate_self_bad_snek) printf("_i_OHNOES_ate_self_bad_snek ");
+	if (ded_snek) printf("ded_snek ");
+	if (_o_DED) printf("_o_DED ");
+	if (input_processing_loop) printf("input_processing_loop ");
+	if (_i_input_received) printf("_i_input_received ");
+	if (reorient_snek) printf("reorient_snek ");
+	if (_) printf("_ ");
+	if (ORIENTATION_LOGIC) printf("ORIENTATION_LOGIC ");
+	if (no_change_if_dumb_snek) printf("no_change_if_dumb_snek ");
+	if (snek_left) printf("snek_left ");
+	if (_i_input_left) printf("_i_input_left ");
+	if (_i_input_right) printf("_i_input_right ");
+	if (snek_up) printf("snek_up ");
+	if (_i_input_up) printf("_i_input_up ");
+	if (_i_input_down) printf("_i_input_down ");
+	if (snek_down) printf("snek_down ");
+	if (yes_change_if_big_brain_snek) printf("yes_change_if_big_brain_snek ");
+	if (MOVEMENT_LOGIC) printf("MOVEMENT_LOGIC ");
+	if (_1_movements) printf("_1_movements ");
+	if (snek_moved) printf("snek_moved ");
+	if (minus_1_movements) printf("minus_1_movements ");
+	if (minus_snek_x) printf("minus_snek_x ");
+	if (minus_snek_y) printf("minus_snek_y ");
+}
 
 
 /* https://stackoverflow.com/questions/1022957/getting-terminal-width-in-c */
@@ -34,13 +88,14 @@ void get_term_size() {
 
     /* establish game size (-2 to account for borders) */
     game_w = (MAX_W < term_w-2) ? (MAX_W) : (term_w-2);
-    game_h = (MAX_H < term_h-3) ? (MAX_H) : (term_h-3);
+    game_h = (MAX_H < term_h-5) ? (MAX_H) : (term_h-5);
 }
 
+/* https://viewsourcecode.org/snaptoken/kilo/02.enteringRawMode.html */
 /* https://codereview.stackexchange.com/questions/292621/get-terminal-size-enable-and-disable-terminal-raw-mode-without-ncurses */
 int start_term_raw_mode() {
     /* save old attributes */
-    if (tcgetattr(STDIN_FILENO, &original_term_attrs) == -1) {
+    if (tcgetattr(STDOUT_FILENO, &original_term_attrs) == -1) {
         return -1;
     }
 
@@ -48,8 +103,8 @@ int start_term_raw_mode() {
 
     /* input modes - no break, no CR to NL, no parity check, no strip char,
      * no start/stop output control. */
-    new_attrs.c_iflag &= ~(0u | IGNBRK | BRKINT | PARMRK | INPCK | ISTRIP | ICRNL | 
-                    INLCR | IGNCR | IXON);
+    new_attrs.c_iflag &= ~(0u | IGNBRK | BRKINT | PARMRK | INPCK | ISTRIP | /*ICRNL | 
+                    INLCR | */ IGNCR | IXON);
 
     /* local modes - echoing off, canonical off, no extended functions, 
      * no signal chars (^Z, ^C) NOTE: acschually no, keep ^C */
@@ -64,17 +119,17 @@ int start_term_raw_mode() {
 
     /* control chars - set return condition: min number of bytes and timer.
      * We want read(2) to return every single byte, without timeout. */
-    new_attrs.c_cc[VMIN] = 1;         /* 1 byte */
-    new_attrs.c_cc[VTIME] = 0;        /* No timer */ /* TODO: we might actually want this? */
+    new_attrs.c_cc[VMIN] = 0;
+    new_attrs.c_cc[VTIME] = 1;
 
     /* Change attributes when output has drained; also flush pending input. */
-    return tcsetattr(STDIN_FILENO, TCSAFLUSH, &new_attrs) == -1
+    return tcsetattr(STDOUT_FILENO, TCSAFLUSH, &new_attrs) == -1
         ? -2 
         : 0;
 }
 
 int stop_term_raw_mode() {
-    return tcsetattr(STDIN_FILENO, TCSAFLUSH, &original_term_attrs) == -1
+    return tcsetattr(STDOUT_FILENO, TCSAFLUSH, &original_term_attrs) == -1
         ? -2 
         : 0;
 }
@@ -151,8 +206,70 @@ void check_ohnoes_ate_self() {
     else _i_no_eat_self_good_snek = 1;
 }
 
+/* https://stackoverflow.com/questions/17167949/how-to-use-timer-in-c */
+int is_time_for_next_frame() {
+    clock_t diff = clock() - last_frame; 
+    int msec = diff * 1000 / CLOCKS_PER_SEC;
+    if (msec > MSEC_PER_FRAME) {
+        _i_next_frame = 1;
+        return 1;
+    }
+    return 0;
+}
+
+/* returns nonzero if we should stop polling input */
+int decipher_input(char* input) {
+    if (input[0] == 27 && input[2] == 0) {
+        /* esc was pressed, stop */
+        stop_term_raw_mode();
+        exit(0);
+    }
+    /* wsad, nehr, up down left right */
+    else if (input[0] == 119 || input[0] == 110 || (input[0] == 27 && input[2] == 65))
+        _i_input_up = 1;
+    else if (input[0] == 115 || input[0] == 101 || (input[0] == 27 && input[2] == 66))
+        _i_input_down = 1;
+    else if (input[0] == 97 || input[0] == 104 || (input[0] == 27 && input[2] == 68))
+        _i_input_left = 1;
+    else if (input[0] == 100 || input[0] == 114 || (input[0] == 27 && input[2] == 67))
+        _i_input_right = 1;
+    else
+        return 0;
+    _i_input_received = 1;
+    return 1;
+}
+
+void poll_input() {
+    char input[3];
+    while (!is_time_for_next_frame())
+    {
+        input[0] = 0;
+        input[1] = 0;
+        input[2] = 0;
+        read(STDIN_FILENO, &input, 1); /* we need 3 bytes for things like arrows */
+        if (input[0]) {
+            if (input[0] == 27) {
+                read(STDIN_FILENO, &input[1], 2); /* we need 3 bytes for things like arrows */
+            }
+            /* printf("%d,%d,%d", input[0], input[1], input[2]); */
+            if (decipher_input(&input[0])) return;
+        }
+    }
+}
+
+void debug_lines() {
+    move_cursor(0, game_h + 5);
+    printf("\033[0K");
+    move_cursor(0, game_h + 4);
+    printf("\033[0K");
+    move_cursor(0, game_h + 3);
+    printf("\033[0K");
+    printf("%d ", steps);
+    printout();
+}
+
 int main() {
-    int steps = 0;
+    setbuf(stdout, NULL);
     srand((unsigned)clock());
     while (!_o_DED) {
         step();
@@ -165,6 +282,9 @@ int main() {
         if (_o_generate_food) generate_food();
         if (_o_check_food) check_food();
         if (_o_check_ohnoes_ate_self) check_ohnoes_ate_self();
+        if (_o_clear_last_tail) erase_snake_tail();
+
+        if (frame_rendered) last_frame = clock();
 
         /* the cells need to record which direction to go next so we can track
          * where to move tail, so we'll just listen in for these */
@@ -173,8 +293,11 @@ int main() {
         if (move_snek && snek_left) cells[snek_y][snek_x] = 3;
         if (move_snek && snek_up) cells[snek_y][snek_x] = 4;
 
-        break;
-        if (steps > 100) { break; }
+        if (input_processing_loop) poll_input();
+        debug_lines();
+
+        /* break; */
+        /* if (steps > 100) { break; } */
     }
     /* printout(); */
     move_cursor(game_w+2, game_h+2);
